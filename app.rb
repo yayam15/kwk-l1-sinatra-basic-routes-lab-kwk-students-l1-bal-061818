24bfeee87273e44e84cd928b40c6e6cefc96c0b5
yayam15 <yazmyn.a.aguilar04@gmail.com>
2018-06-26 15:59:26 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,15 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/'name do
+  get '/name' do
     "My name is Yazmyn!"
 end
 
-  get '/' hometown do
+  get '/hometown' do
     "My hometown is owings Mills, Maryland"
   end
 
-  get '/' favorite-song do
+  get '/favorite-song' do
     "My favorite song is IDK"
   end
 end
